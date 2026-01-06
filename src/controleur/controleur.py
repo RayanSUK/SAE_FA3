69408b4a42f7b427e1a653dcce7aacd6a1049b07
Rayan <sukkarie.rayan@gmail.com>
2026-01-06 14:01:40 +0100

    alignement avec la nouvelle version (pour lancer au moins l'app)

--- src/controleur/controleur.py
+++ src/controleur/controleur.py
@@ -1,30 +1,24 @@
-from src.modele.graphe import ModeleGraphe
 from src.vue.vue_principale import VueApplication
+from src.modele.graphe import Graphe, Couleur
 
 
 class ControleurApplication:
     def __init__(self, root):
-        self.modele = ModeleGraphe()
+        # Graphe minimal
+        self.graphe = Graphe(sommets={}, voisins={})
         self.vue = VueApplication(root)
         self.lier_evenements()
 
     def lier_evenements(self):
         v = self.vue
 
-        # Clics sur les cases couleurs
+        # Sélection du type de sommet
         v.tuile_noir.bind("<Button-1>", self.clic_case_noire)
         v.tuile_blanc.bind("<Button-1>", self.clic_case_blanche)
         v.tuile_bleu.bind("<Button-1>", self.clic_case_bleue)
         v.tuile_vert.bind("<Button-1>", self.clic_case_verte)
         v.tuile_jaune.bind("<Button-1>", self.clic_case_jaune)
 
-        # Sélection d'algorithme
-        v.liste_algo.bind("<<ComboboxSelected>>", self.quand_algo_change)
-
-        # Boutons
-        v.bouton_lancer_algo.config(command=self.lancer_algo)
-        v.bouton_effacer_resultat.config(command=self.effacer_resultat)
-        v.bouton_effacer_tout.config(command=self.effacer_tout)
 
     def clic_case_noire(self, event):
         pass
@@ -39,17 +33,5 @@ class ControleurApplication:
         pass
 
     def clic_case_jaune(self, event):
-        pass
-
-    def quand_algo_change(self, event):
-        pass
-
-    def lancer_algo(self):
-        pass
-
-    def effacer_resultat(self):
-        pass
-
-    def effacer_tout(self):
         pass
 
